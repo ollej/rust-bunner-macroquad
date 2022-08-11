a160e6bd4e75a516079e736021414dc75cc1f995
Olle Wreede <olle@wreede.se>
2022-08-11 15:50:12 +0200

    Add some unit tests

---
+++ tests/grass_tests.rs
@@ -0,0 +1,59 @@
+use bunner_macroquad::grass::Grass;
+use bunner_macroquad::hedge_mask::HedgeMask;
+use macroquad::rand::srand;
+
+#[test]
+fn generate_hedge_mask_length() {
+    let mask = Grass::generate_hedge_mask();
+    assert_eq!(mask.len(), 14);
+}
+
+#[test]
+fn generate_hedge_mask_beginning_hole() {
+    srand(42);
+    let mask = Grass::generate_hedge_mask();
+    assert_eq!(
+        mask,
+        vec![
+            HedgeMask::Empty,
+            HedgeMask::Empty,
+            HedgeMask::Empty,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+        ]
+    );
+}
+
+#[test]
+fn generate_hedge_mask_middle_hole() {
+    srand(1);
+    let mask = Grass::generate_hedge_mask();
+    assert_eq!(
+        mask,
+        vec![
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Empty,
+            HedgeMask::Empty,
+            HedgeMask::Empty,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+            HedgeMask::Hedge,
+        ]
+    );
+}
